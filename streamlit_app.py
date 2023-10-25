9a3ebd42b16cccb68395ad236a144d7f5fc08698
123114-sekine <sekine_kazuma@comture.com>
2023-10-25 12:06:39 +0900

    'uploader'

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,25 +22,24 @@ if uploaded_file is not None:
         [0, 3, 4, 5, 6],
         [0, 3, 4, 5, 6])
 
-if len(usecols) == 0 or len(names) == 0:
-    st.error('解析対象の列が指定されていません。')
-    
-    help_txt = '''
-        以下のフォーマット文字列を解析可能です。詳細については、[公式ページ](https://httpd.apache.org/docs/2.4/ja/mod/mod_log_config.html)を参照して下さい。
+    if len(usecols) == 0 or len(names) == 0:
+        st.error('解析対象の列が指定されていません。')
 
-        | 列名 | フォーマット文字列 | 説明 | 
-        |:-----|:-----:|:-----|
-        | Remote Host | `%h` | リモートホスト |
-        | Time | `%t` | リクエストを受付けた時刻 | 
-        | Request | `\"%r\"` | リクエストの最初の行 | 
-        | Status | `%>s` | ステータス | 
-        | Size | `%b` | レスポンスのバイト数 | 
-        | User Agent | `\"%{User-agent}i\"` | リクエストのUser-agentヘッダの内容 | 
-        | Response Time | `%D` または `%T` | リクエストを処理するのにかかった時間 |         
-        '''
+        help_txt = '''
+            以下のフォーマット文字列を解析可能です。詳細については、[公式ページ](https://httpd.apache.org/docs/2.4/ja/mod/mod_log_config.html)を参照して下さい。
 
-    names = st.multiselect(
-        'これらの列を何を意味しますか？',
-        ['Remote Host', 'Time', 'Request', 'Status', 'Size', 'User Agent', 'Response Time'],
-        help=help_txt)
+            | 列名 | フォーマット文字列 | 説明 | 
+            |:-----|:-----:|:-----|
+            | Remote Host | `%h` | リモートホスト |
+            | Time | `%t` | リクエストを受付けた時刻 | 
+            | Request | `\"%r\"` | リクエストの最初の行 | 
+            | Status | `%>s` | ステータス | 
+            | Size | `%b` | レスポンスのバイト数 | 
+            | User Agent | `\"%{User-agent}i\"` | リクエストのUser-agentヘッダの内容 | 
+            | Response Time | `%D` または `%T` | リクエストを処理するのにかかった時間 |         
+            '''
 
+        names = st.multiselect(
+            'これらの列を何を意味しますか？',
+            ['Remote Host', 'Time', 'Request', 'Status', 'Size', 'User Agent', 'Response Time'],
+            default_names, help=help_txt)
