1015b8bbb6b0b64a8a09e8ff4610a4e3ddedac73
123114-sekine <sekine_kazuma@comture.com>
2023-10-25 12:39:02 +0900

    'default_namesとは'

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,8 @@ if uploaded_file is not None:
         '何番目の列を解析の対象にしますか？',
         [0, 3, 4, 5, 6],
         [0, 3, 4, 5, 6])
-
-    if len(usecols) == 0 or len(names) == 0:
-        st.error('解析対象の列が指定されていません。')
-
-        help_txt = '''
+    
+    help_txt = '''
             以下のフォーマット文字列を解析可能です。詳細については、[公式ページ](https://httpd.apache.org/docs/2.4/ja/mod/mod_log_config.html)を参照して下さい。
 
             | 列名 | フォーマット文字列 | 説明 | 
@@ -39,7 +36,15 @@ if uploaded_file is not None:
             | Response Time | `%D` または `%T` | リクエストを処理するのにかかった時間 |         
             '''
 
-        names = st.multiselect(
+    names = st.multiselect(
             'これらの列を何を意味しますか？',
             ['Remote Host', 'Time', 'Request', 'Status', 'Size', 'User Agent', 'Response Time'],
             default_names, help=help_txt)
+
+    if len(usecols) == 0 or len(names) == 0:
+        st.error('解析対象の列が指定されていません。')
+
+
+        
+
+
