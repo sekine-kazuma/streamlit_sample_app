b271ade4dbd1bd949dc3552e32b3c55a731bcdb2
123114-sekine <sekine_kazuma@comture.com>
2023-10-25 15:36:18 +0900

    'map'

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,10 @@ if uploaded_file is not None:
     if len(usecols) == 0 or len(names) == 0:
         st.error('解析対象の列が指定されていません。')
 
+st.map(df)
+my_bar=st.progress(20)
+my_bar.progress(20)
+
 
         
 
